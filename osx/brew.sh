c61fb1231002524496f53ee526e05847b430ae50
Craig Rosie <craigrosie7@gmail.com>
2016-05-10 17:08:54 +0100

    OSX: Install Source Code Pro font

--- osx/brew.sh
+++ osx/brew.sh
@@ -113,5 +113,9 @@ brew install gs
 # Install graphviz for pyreverse
 brew install graphviz
 
+# Install Source Code Pro font
+brew tap caskroom/fonts
+brew cask install font-source-code-pro
+
 # Cleanup
 brew cleanup
