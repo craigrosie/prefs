bf7c34f04a0c8ff00cfa9d775de351beb92b201c
Craig Rosie <craigrosie7@gmail.com>
2022-11-28 22:31:04 +0000

    NEOVIM: Change keymap for vim.diagnostic.open_float

--- neovim/init.lua
+++ neovim/init.lua
@@ -139,7 +139,7 @@ require('mason-lspconfig').setup({
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, opts)
