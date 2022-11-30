0a7a22d8adeadbef2751e4a6ec6e3216bb3aaca7
Craig Rosie <craigrosie7@gmail.com>
2022-11-30 22:06:03 +0000

    NEOVIM: Format-on-save using null-ls

--- neovim/init.lua
+++ neovim/init.lua
@@ -302,6 +302,23 @@ null_ls.setup({
       null_ls.builtins.formatting.black,
     },
 })
+-- format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+})
+
 
 -- gitsigns
 require('gitsigns').setup()
