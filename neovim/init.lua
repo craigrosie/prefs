216c1bb9fbe319b496f7b7e7357f968697529a65
Craig Rosie <craigrosie7@gmail.com>
2023-01-22 18:06:45 +0000

    NEOVIM: Add neovim-session-manager config

--- neovim/init.lua
+++ neovim/init.lua
@@ -532,6 +532,11 @@ require('mini.pairs').setup()
 require('mini.statusline').setup()
 require('mini.surround').setup()
 
+-- neovim-session-manager
+require('session_manager').setup({
+  autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir
+})
+
 -- =================================================================================================
 
 -- KEY MAPPINGS
