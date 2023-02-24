987977b7fac04225efd9dfde0c128d0e04c3055f
Craig Rosie <craigrosie7@gmail.com>
2023-02-24 10:57:43 +0000

    NEOVIM: Fix null-ls format on save

--- neovim/init.lua
+++ neovim/init.lua
@@ -356,30 +356,26 @@ require('dap-python').test_runner = 'pytest'
 
 -- null-ls
 local null_ls = require("null-ls")
-
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
     sources = {
       null_ls.builtins.formatting.isort,
       null_ls.builtins.formatting.black,
     },
+    -- format on save
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd({"BufWritePre"}, {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
+      end
+    end,
 })
--- format on save
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-require("null-ls").setup({
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
-})
-
 
 -- gitsigns
 require('gitsigns').setup()
