c0b02bf363189dc3e9ceb0839320460e018bd352
Craig Rosie <craigrosie7@gmail.com>
2023-04-06 17:06:49 +0100

    NEOVIM: Update null-ls sources

--- neovim/init.lua
+++ neovim/init.lua
@@ -409,23 +409,32 @@ dap.configurations.python = {
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-    sources = {
-      null_ls.builtins.formatting.isort,
-      null_ls.builtins.formatting.black,
-    },
-    -- format on save
-    on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd({"BufWritePre"}, {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ bufnr = bufnr })
-          end,
-        })
-      end
-    end,
+  sources = {
+    -- formatting
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.lua_format,
+    null_ls.builtins.formatting.ruff,
+    null_ls.builtins.formatting.taplo,
+    -- linting
+    null_ls.builtins.diagnostics.checkmake,
+    null_ls.builtins.diagnostics.ruff,
+    -- code actions
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.code_actions.shellcheck,
+  },
+  -- format on save
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
 
 -- gitsigns
