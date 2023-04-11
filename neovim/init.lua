379eb4bba58e88fbe319627b3754e17b18ae8e40
Craig Rosie <craigrosie7@gmail.com>
2023-04-11 17:04:28 +0100

    NEOVIM: Set vim.g.editorconfig = false

--- neovim/init.lua
+++ neovim/init.lua
@@ -78,6 +78,9 @@ vim.opt.sidescrolloff = 3
 vim.opt.smartcase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+-- Completely disable editorconfig integration, as the max_line_length field
+-- sets `textwidth` which is never what I want
+vim.g.editorconfig = false
 -- ==================================================================================================
 -- Autocmds
 
