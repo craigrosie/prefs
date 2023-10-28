b5b0701ed641da66736dca0721438d850d44b44b
Craig Rosie <craigrosie7@gmail.com>
2023-10-28 10:18:15 +0100

    NEOVIM: Add <C-e> terminal keymap to enter normal mode

--- neovim/init.lua
+++ neovim/init.lua
@@ -942,6 +942,7 @@ vim.keymap.set('t', '<C-h>', "<C-\\><C-n><C-w>h")
 vim.keymap.set('t', '<C-j>', "<C-\\><C-n><C-w>j")
 vim.keymap.set('t', '<C-k>', "<C-\\><C-n><C-w>k")
 vim.keymap.set('t', '<C-l>', "<C-\\><C-n><C-w>l")
+vim.keymap.set('t', '<C-e>', "<C-\\><C-n>")
 
 -- UltiSnips
 vim.g.UltiSnipsSnippetDirectories = { "UltiSnips", "custom_snippets" }
