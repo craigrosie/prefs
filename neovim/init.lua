b88e447fe827fb5b103f750bccbcba0ae15e2470
Craig Rosie <craigrosie7@gmail.com>
2023-10-29 09:32:02 +0000

    NEOVIM: Add emmet-ls config

--- neovim/init.lua
+++ neovim/init.lua
@@ -243,13 +243,49 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
+local lspconfig = require('lspconfig')
 for i in pairs(language_servers) do
   local server = language_servers[i]
-  require('lspconfig')[server].setup({
+  lspconfig[server].setup({
     on_attach = on_attach,
     flags = lsp_flags,
   })
 end
+lspconfig.emmet_language_server.setup({
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "jinja",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "typescriptreact",
+    "vue",
+  },
+  -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+  -- **Note:** only the options listed in the table are supported.
+  init_options = {
+    --- @type string[]
+    excludeLanguages = {},
+    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+    preferences = {},
+    --- @type boolean Defaults to `true`
+    showAbbreviationSuggestions = true,
+    --- @type "always" | "never" Defaults to `"always"`
+    showExpandedAbbreviation = "always",
+    --- @type boolean Defaults to `false`
+    showSuggestionsAsSnippets = false,
+    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+    syntaxProfiles = {},
+    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+    variables = {},
+  },
+})
 
 -- NOTE: Copilot needs to be set up before cmp
 require("copilot").setup({
