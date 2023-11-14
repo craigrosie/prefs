75ddf8523b1687fd5d4419a957904422896d732b
Craig Rosie <craigrosie7@gmail.com>
2023-11-14 08:34:24 +0000

    NEOVIM: Don't show current context in indent-blankline

--- neovim/init.lua
+++ neovim/init.lua
@@ -916,7 +916,7 @@ require("neoscroll").setup()
 
 require("indent_blankline").setup{
   show_current_context = true,
-  show_current_context_start = true,
+  show_current_context_start = false,
 }
 
 -- nvim-pytrize
