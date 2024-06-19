c33861c45482a075a36eeaa91dbc0439d1191081
Craig Rosie <craigrosie7@gmail.com>
2024-06-19 17:33:54 +0100

    NEOVIM: Enable nvim-lastplace

--- neovim/lua/custom/plugins/lastplace.lua
+++ neovim/lua/custom/plugins/lastplace.lua
@@ -1,7 +1,6 @@
 return {
   'ethanholz/nvim-lastplace',
   event = 'VeryLazy',
-  enabled = false,
   opts = {
     lastplace_ignore_buftype = { 'quickfix', 'nofile', 'help', 'terminal' },
     lastplace_ignore_filetype = { 'gitcommit', 'gitrebase', 'fzf' },
