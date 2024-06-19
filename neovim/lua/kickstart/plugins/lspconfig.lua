0246689a17d43e2a76dda43f5137a10e939b5ddd
Craig Rosie <craigrosie7@gmail.com>
2024-06-19 17:26:03 +0100

    NEOVIM: Add shellcheck to ensure_installed

--- neovim/lua/kickstart/plugins/lspconfig.lua
+++ neovim/lua/kickstart/plugins/lspconfig.lua
@@ -239,6 +239,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'shellcheck',
       })
       require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
