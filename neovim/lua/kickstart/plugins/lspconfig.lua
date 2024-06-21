0c8e153a3589b44c1efa8ce626ec1206f7685841
Craig Rosie <craigrosie7@gmail.com>
2024-06-21 13:50:16 +0100

    NEOVIM: Add keymap to go-to-definition in vsplit

--- neovim/lua/kickstart/plugins/lspconfig.lua
+++ neovim/lua/kickstart/plugins/lspconfig.lua
@@ -98,6 +98,8 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<C-w>d', '<Cmd>vsp<CR><Cmd>lua vim.lsp.buf.definition()<CR>', 'Go to [D]efinition in vsplit')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
