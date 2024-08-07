ae35fac92db9ec160b11eac119f140f797b78969
Craig Rosie <craigrosie7@gmail.com>
2024-08-07 11:14:27 +0100

    NEOVIM: Use lualine instead of mini.statusline

---
+++ neovim/lua/custom/plugins/lualine.lua
@@ -0,0 +1,49 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'codedark',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = { statusline = {}, winbar = {} },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
+    },
+    sections = {
+      lualine_a = { {
+        'mode',
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+      } },
+      lualine_b = {
+        -- { 'branch', cond = min_window_width(80) },
+        -- 'diff',
+        -- 'diagnostics',
+      },
+      lualine_c = { { 'filename', path = 1 } },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { { 'filename', path = 1 } },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = { 'fzf', 'nvim-dap-ui', 'nvim-tree', 'toggleterm' },
+  },
+}
