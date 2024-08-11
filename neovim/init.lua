3eefb3da96ff06a82d48ade91facf1f10031d639
Craig Rosie <craigrosie7@gmail.com>
2024-08-11 22:12:24 +0100

    NEOVIM: init.lua formatting

--- neovim/init.lua
+++ neovim/init.lua
@@ -95,25 +95,25 @@ vim.g.have_nerd_font = true
 
 -- Check if IS_WORK env variable is set
 local function is_work_env_set()
-  return os.getenv 'IS_WORK' ~= nil
+  return os.getenv('IS_WORK') ~= nil
 end
--- Store the result so it can be used to conditionally enable plugins
+-- store the result so it can be used to conditionally enable plugins
 vim.g.is_work_env_set = is_work_env_set()
 
 -- [[ Setting options ]]
-require 'options'
+require('options')
 
 -- [[ Custom autocmds ]]
-require 'autocmds'
+require('autocmds')
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require('keymaps')
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require('lazy-bootstrap')
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require('lazy-plugins')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
