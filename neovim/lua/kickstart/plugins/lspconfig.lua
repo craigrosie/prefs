04dedfb4c4180d6e87ad7f23d03cb410a4164c1c
Craig Rosie <craigrosie7@gmail.com>
2024-08-19 13:30:04 +0100

    NEOVIM: Add hadolint and prettierd to mason-tool-installer ensure_installed

--- neovim/lua/kickstart/plugins/lspconfig.lua
+++ neovim/lua/kickstart/plugins/lspconfig.lua
@@ -240,9 +240,11 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'hadolint',
         'stylua', -- Used to format Lua code
         'shellcheck',
         'eslint_d',
+        'prettierd',
       })
       require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
