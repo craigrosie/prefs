ee1e8c92e002560469cd3347176e6a834921302f
Craig Rosie <craigrosie7@gmail.com>
2024-08-28 11:14:25 +0100

    NEOVIM: Move yank highlight autocmd from keymaps to autocmds

--- neovim/lua/autocmds.lua
+++ neovim/lua/autocmds.lua
@@ -1,3 +1,17 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- Set filetype to snippets for .snippets files
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = '*.snippets',
--- neovim/lua/keymaps.lua
+++ neovim/lua/keymaps.lua
@@ -58,18 +58,6 @@ vim.keymap.set('n', '<C-w>t', ':tab split<cr>', { desc = 'Open current buffer in
 vim.keymap.set('n', 'j', 'gj', { desc = 'Move linewise even on wrapped lines' })
 vim.keymap.set('n', 'k', 'gk', { desc = 'Move linewise even on wrapped lines' })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 -- vim: ts=2 sts=2 sw=2 et
