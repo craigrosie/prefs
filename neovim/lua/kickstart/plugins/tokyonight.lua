7d945663748adb41a447825523e898e4a630156e
Craig Rosie <craigrosie7@gmail.com>
2024-09-26 22:24:17 +0100

    NEOVIM: Make tokyonight colours more vibrant

--- neovim/lua/kickstart/plugins/tokyonight.lua
+++ neovim/lua/kickstart/plugins/tokyonight.lua
@@ -15,6 +15,39 @@ return {
       -- You can configure highlights by doing something like:
       vim.cmd.hi('Comment gui=none')
     end,
+    opts = {
+      -- Make tokyonight colours a bit more vibrant
+      on_colors = function(colors)
+        local hsluv = require('tokyonight.hsluv')
+        local multiplier = 2.0
+
+        for k, v in pairs(colors) do
+          if type(v) == 'string' and v ~= 'NONE' then
+            local hsv = hsluv.hex_to_hsluv(v)
+            hsv[2] = hsv[2] * multiplier > 100 and 100 or hsv[2] * multiplier
+            colors[k] = hsluv.hsluv_to_hex(hsv)
+          elseif type(v) == 'table' then
+            if vim.islist(v) then
+              for kk, vv in ipairs(v) do
+                if type(vv) == 'string' and vv ~= 'NONE' then
+                  local hsv = hsluv.hex_to_hsluv(vv)
+                  hsv[2] = hsv[2] * multiplier > 100 and 100 or hsv[2] * multiplier
+                  colors[k][kk] = hsluv.hsluv_to_hex(hsv)
+                end
+              end
+            else
+              for kk, vv in pairs(v) do
+                if type(vv) == 'string' and vv ~= 'NONE' then
+                  local hsv = hsluv.hex_to_hsluv(vv)
+                  hsv[2] = hsv[2] * multiplier > 100 and 100 or hsv[2] * multiplier
+                  colors[k][kk] = hsluv.hsluv_to_hex(hsv)
+                end
+              end
+            end
+          end
+        end
+      end,
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
