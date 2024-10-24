73d0ecb969158c809e37017dee27e28bd2818b58
Craig Rosie <craigrosie7@gmail.com>
2024-10-24 08:41:58 +0100

    NEOVIM: Refactor codecompanion config

--- neovim/lua/custom/plugins/codecompanion.lua
+++ neovim/lua/custom/plugins/codecompanion.lua
@@ -1,5 +1,26 @@
 local utils = require('utils')
 
+local format_lines = function(prompt_lines, ...)
+  local arguments = { ... }
+
+  -- Ensure that all empty arguments are replaced with '%s', since that allows
+  -- us to sanitize them.
+  for idx, argument in ipairs(arguments) do
+    if argument == '' then
+      arguments[idx] = '%s'
+    end
+  end
+
+  local prompt = table.concat(prompt_lines, '\n')
+
+  if #arguments > 0 then
+    prompt = prompt:format(unpack(arguments))
+  end
+
+  -- Remove any left over '%s', including the newline, from the prompt.
+  return prompt:gsub('%%s\n', '')
+end
+
 return {
   'olimorris/codecompanion.nvim',
   enabled = utils.is_plugin_enabled('NVIM_ENABLE_CODE_COMPANION'),
@@ -50,61 +71,56 @@ return {
           },
         },
       },
-      pre_defined_prompts = {
+      prompt_library = {
         -- Have to set mappings for these because disabling them (setting to nil) doesn't seem to work
         ['Custom Prompt'] = {
           opts = {
-            mapping = ';custom',
+            mapping = false,
           },
         },
         ['Explain'] = {
           opts = {
-            mapping = ';explain',
+            mapping = false,
           },
         },
         ['Unit Tests'] = {
           opts = {
-            mapping = ';test',
+            mapping = false,
           },
         },
         ['Fix code'] = {
           opts = {
-            mapping = ';fix',
+            mapping = false,
           },
         },
         ['Buffer selection'] = {
           opts = {
-            mapping = ';buffer',
+            mapping = false,
           },
         },
         ['Explain LSP Diagnostics'] = {
           opts = {
-            mapping = ';diag',
+            mapping = false,
           },
         },
         ['Generate a Commit Message'] = {
-          strategy = 'chat',
-          description = 'Generate a commit message',
           opts = {
-            index = 9,
-            default_prompt = true,
-            mapping = ';commit',
-            slash_cmd = 'commit',
-            auto_submit = true,
+            mapping = false,
+            slash_cmd = false,
           },
           prompts = {
             {
               role = 'user',
               content = function()
-                return string.format(
-                  [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
-
-```diff
-%s
-```
-]],
-                  vim.fn.system('git diff --staged')
-                )
+                -- return string.format(
+                return format_lines({
+                  'You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:',
+                  '',
+                  '```diff',
+                  '%s',
+                  '```',
+                  ']],',
+                }, vim.fn.system('git diff --staged'))
               end,
               opts = {
                 contains_code = true,
@@ -112,14 +128,13 @@ return {
             },
           },
         },
+        -- Custom prompts
         ['Review this code'] = {
           strategy = 'chat',
           description = 'Review this code and suggest improvements',
           opts = {
             index = 10,
             default_prompt = false,
-            mapping = ';review',
-            slash_cmd = 'review',
             auto_submit = true,
           },
           prompts = {
@@ -128,25 +143,88 @@ return {
               content = function(context)
                 local code = require('codecompanion.helpers.actions').get_code(context.start_line, context.end_line)
 
-                return string.format(
-                  [[I want you to act as a senior %s developer. I want you to review the selected code, then suggest improvements and provide feedback. Please consider the following points:
-
-1. Only use idiomatic code and best practices for the language.
-2. Suggest improvements to the code.
-3. Favour deep modules/functions, based on principles of "A Philosophy of Software Design" by John Ousterhout.
-4. Suggest better names for variables, functions or classes, if appropriate.
-5. Suggest better abstractions, if appropriate.
-6. Suggest better error handling, if appropriate.
-7. Only make suggestions if you are confident that they actually improve the code. Don't suggest changes just for the sake of it.
-
-```%s
-%s
-```
-]],
-                  context.filetype,
-                  context.filetype,
-                  code
-                )
+                return format_lines({
+                  'I want you to act as a senior %s developer. I want you to review the selected code, then suggest improvements and provide feedback. Please consider the following points:',
+                  '',
+                  '1. Only use idiomatic code and best practices for the language.',
+                  '2. Suggest improvements to the code.',
+                  '3. Favour deep modules/functions, based on principles of "A Philosophy of Software Design" by John Ousterhout.',
+                  '4. Suggest better names for variables, functions or classes, if appropriate.',
+                  '5. Suggest better abstractions, if appropriate.',
+                  '6. Suggest better error handling, if appropriate.',
+                  "7. Only make suggestions if you are confident that they actually improve the code. Don't suggest changes just for the sake of it.",
+                  '',
+                  '```%s',
+                  '%s',
+                  '```',
+                }, context.filetype, context.filetype, code)
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
+        ['Review PR code'] = {
+          strategy = 'chat',
+          description = 'Review PR code',
+          opts = {
+            index = 11,
+            default_prompt = true,
+            auto_submit = true,
+          },
+          prompts = {
+            {
+              role = 'user',
+              content = function()
+                return format_lines({
+                  'I want you to act as a senior developer. I want you to review the selected code as if you were reviewing a pull request, then suggest improvements and provide feedback. Please consider the following points:',
+                  '',
+                  '1. Only use idiomatic code and best practices for the language.',
+                  '2. Suggest improvements to the code.',
+                  '3. Favour deep modules/functions, based on principles of "A Philosophy of Software Design" by John Ousterhout.',
+                  '4. Suggest better names for variables, functions or classes, if appropriate.',
+                  '5. Suggest better abstractions, if appropriate.',
+                  '6. Suggest better error handling, if appropriate.',
+                  '7. Look out for typos or spelling mistakes.',
+                  "8. Only make suggestions if you are confident that they actually improve the code. Don't suggest changes just for the sake of it.",
+                  '',
+                  '```diff',
+                  '%s',
+                  '```',
+                }, vim.fn.system('git diff origin/main...HEAD'))
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
+        ['PR Description'] = {
+          strategy = 'chat',
+          description = 'Generate a PR description',
+          opts = {
+            index = 12,
+            default_prompt = true,
+            auto_submit = true,
+          },
+          prompts = {
+            {
+              role = 'user',
+              content = function()
+                return format_lines({
+                  'I want you to act as a senior developer. I want you to generate a pull request description for the changes you have made. Please consider the following points:',
+                  '',
+                  'Your task is to generate a very concise and structured pull request description based on the provided diffs. It must be easy to parse for reviewers.',
+                  'The description should always include:',
+                  '- A summary of the changes under a ### Summary heading.',
+                  '- A bulleted list of the main changes under a ### Key Changes heading.',
+                  'Avoid redundant information.',
+                  '',
+                  '```diff',
+                  '%s',
+                  '```',
+                }, vim.fn.system('git diff origin/main...HEAD'))
               end,
               opts = {
                 contains_code = true,
@@ -156,13 +234,14 @@ return {
         },
       },
     })
+
     vim.keymap.set('v', '<leader>cl', "<cmd>'<,'>CodeCompanion<cr>", { desc = '[c]odecompanion in[l]ine prompt' })
     vim.keymap.set('v', '<leader>cf', '<cmd>CodeCompanion /lsp<cr>', { desc = '[c]odecompanion [f]ix diagnostics' })
     vim.keymap.set('v', '<leader>ca', '<cmd>CodeCompanionAdd<cr>', { desc = '[c]odecompanion add selection to chat' })
     vim.keymap.set(
       { 'n', 'v' },
       '<leader>cc',
-      '<cmd>CodeCompanionToggle<cr>',
+      '<cmd>CodeCompanionChat Toggle<cr>',
       { desc = '[c]odecompanion toggle [c]hat' }
     )
     vim.keymap.set(
