3b2f9de86d7ff29bf877e8f770354ef24dae8ef6
Craig Rosie <craigrosie7@gmail.com>
2024-10-25 15:44:24 +0100

    NEOVIM: Remove luasnip config from cmp

--- neovim/lua/kickstart/plugins/cmp.lua
+++ neovim/lua/kickstart/plugins/cmp.lua
@@ -5,33 +5,6 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      -- {
-      --   'L3MON4D3/LuaSnip',
-      --   build = (function()
-      --     -- Build Step is needed for regex support in snippets.
-      --     -- This step is not supported in many windows environments.
-      --     -- Remove the below condition to re-enable on windows.
-      --     if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-      --       return
-      --     end
-      --     return 'make install_jsregexp'
-      --   end)(),
-      --   dependencies = {
-      --     -- `friendly-snippets` contains a variety of premade snippets.
-      --     --    See the README about individual language/framework/plugin snippets:
-      --     --    https://github.com/rafamadriz/friendly-snippets
-      --     -- {
-      --     --   'rafamadriz/friendly-snippets',
-      --     --   config = function()
-      --     --     require('luasnip.loaders.from_vscode').lazy_load()
-      --     --   end,
-      --     -- },
-      --   },
-      -- },
-      --
-      -- 'saadparwaiz1/cmp_luasnip',
-
       -- Adds other completion capabilities.
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
@@ -58,11 +31,10 @@ return {
         { name = 'nvim_lsp_signature_help' },
         { name = 'path' },
         { name = 'buffer', keyword_length = 2 },
-        { name = 'luasnip' },
         { name = 'path' },
       }
       local comparators = {
-        -- Below is the default comparitor list and order for nvim-cmp
+        -- Below is the default comparator list and order for nvim-cmp
         cmp.config.compare.offset,
         -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
         cmp.config.compare.exact,
