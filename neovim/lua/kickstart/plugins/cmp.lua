abeb6943a55540479dd8d9687735ca89fecc31d5
Craig Rosie <craigrosie7@gmail.com>
2024-10-25 15:47:57 +0100

    NEOVIM: Change cmp behaviour on <CR>

--- neovim/lua/kickstart/plugins/cmp.lua
+++ neovim/lua/kickstart/plugins/cmp.lua
@@ -88,7 +88,24 @@ return {
             'i',
             's', --[[ "c" (to enable the mapping in command mode) ]]
           }),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<CR>'] = cmp.mapping({
+            i = function(fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              else
+                fallback()
+              end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            -- c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+            c = function(fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              else
+                fallback()
+              end
+            end,
+          }),
         },
         completion = { completeopt = 'menu,menuone,noselect' },
 
