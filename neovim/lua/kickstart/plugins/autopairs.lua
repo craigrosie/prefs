ef67d44e4fc3e04c6579730f52d1a2fd4cd6402b
Craig Rosie <craigrosie7@gmail.com>
2025-01-30 11:20:50 +0000

    NEOVIM: Fix autopairs triple backticks in codecompanion chat buffer

--- neovim/lua/kickstart/plugins/autopairs.lua
+++ neovim/lua/kickstart/plugins/autopairs.lua
@@ -7,7 +7,17 @@ return {
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
-    require('nvim-autopairs').setup({})
+    local npairs = require('nvim-autopairs')
+    local Rule = require('nvim-autopairs.rule')
+    local cond = require('nvim-autopairs.conds')
+
+    npairs.setup({})
+
+    npairs.add_rules({
+      -- Fix triple backticks in codecompanion chat buffer
+      Rule('```', '```', { 'codecompanion' }):with_pair(cond.not_before_char('`', 3)),
+      Rule('```.*$', '```', { 'codecompanion' }):only_cr():use_regex(true),
+    })
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
