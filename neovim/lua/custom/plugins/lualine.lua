c295bd0112fbc35f4817efffd96141f7509d3050
Craig Rosie <craigrosie7@gmail.com>
2025-03-20 14:12:25 +0000

    NEOVIM: Use tokyonight theme for lualine

--- neovim/lua/custom/plugins/lualine.lua
+++ neovim/lua/custom/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'codedark',
+      theme = 'tokyonight',
       component_separators = { left = '', right = '' },
       section_separators = { left = '', right = '' },
       disabled_filetypes = { statusline = {}, winbar = {} },
