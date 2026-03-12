ff593dbc5445d4959aa912ec801eda42bd4defd4
Craig Rosie <craigrosie7@gmail.com>
2026-03-12 14:29:27 +0000

    NEOVIM: Update statuscol config

--- neovim/lua/custom/plugins/statuscol.lua
+++ neovim/lua/custom/plugins/statuscol.lua
@@ -14,6 +14,19 @@ return {
           click = 'v:lua.ScLa',
         },
       },
+      -- relculright = false,
+      -- segments = {
+      --   { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+      --   {
+      --     sign = { namespace = { 'diagnostic/signs' }, maxwidth = 2, auto = true },
+      --     click = 'v:lua.ScSa',
+      --   },
+      --   { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
+      --   {
+      --     sign = { name = { '.*' }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+      --     click = 'v:lua.ScSa',
+      --   },
+      -- },
     })
   end,
 }
