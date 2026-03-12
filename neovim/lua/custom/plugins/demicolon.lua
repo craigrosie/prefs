1732f6604bbc08de4c270b3441c88ea83011106d
Craig Rosie <craigrosie7@gmail.com>
2026-03-12 14:28:09 +0000

    NEOVIM: Update demicolon config

--- neovim/lua/custom/plugins/demicolon.lua
+++ neovim/lua/custom/plugins/demicolon.lua
@@ -1,22 +1,21 @@
 return {
   'mawkler/demicolon.nvim',
   enabled = true,
-  keys = { ';', ',', 't', 'f', 'T', 'F', ']', '[', ']d', '[d' },
+  keys = { ';', ',', 't', 'f', 'T', 'F', ']', '[' },
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   opts = {
-    integrations = {
-      gitsigns = {
-        enabled = true,
-      },
-      neotest = {
-        enabled = true,
-      },
-      vimtex = {
-        enabled = false,
-      },
+    -- Create default keymaps
+    keymaps = {
+      -- Create t/T/f/F key mappings
+      horizontal_motions = true,
+      -- Create ; and , key mappings. Set it to 'stateless', 'stateful', or false to
+      -- not create any mappings. 'stateless' means that ;/, move right/left.
+      -- 'stateful' means that ;/, will remember the direction of the original
+      -- jump, and `,` inverts that direction (Neovim's default behaviour).
+      repeat_motions = 'stateless',
     },
   },
 }
