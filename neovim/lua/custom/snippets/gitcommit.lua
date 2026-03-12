3e0d0ba4b7beae2f9947b97000ad326255c701f0
Craig Rosie <craigrosie7@gmail.com>
2026-03-12 14:31:14 +0000

    NEOVIM: Update gitcommit snippets

--- neovim/lua/custom/snippets/gitcommit.lua
+++ neovim/lua/custom/snippets/gitcommit.lua
@@ -19,29 +19,73 @@ local types = require('luasnip.util.types')
 local conds = require('luasnip.extras.conditions')
 local conds_expand = require('luasnip.extras.conditions.expand')
 
+local get_ticket = function()
+  local handle = io.popen('git rev-parse --abbrev-ref HEAD')
+  if not handle then
+    return 'Failed to get branch'
+  end
+
+  local branch = handle:read('*a'):gsub('\n', '') -- Read output and remove newline
+  handle:close()
+
+  -- Match the branch name with the pattern
+  local branch_re = '^cr/(%w+)(%-?%d+)'
+  local proj, issue = branch:match(branch_re)
+
+  -- If match found, format the output
+  if proj and issue then
+    return string.upper(proj) .. issue
+  else
+    return 'Invalid branch format'
+  end
+end
+
 return {
-  s('br', {
-    f(function()
-      local handle = io.popen('git rev-parse --abbrev-ref HEAD')
-      if not handle then
-        return 'Failed to get branch'
-      end
-
-      local branch = handle:read('*a'):gsub('\n', '') -- Read output and remove newline
-      handle:close()
-
-      -- Match the branch name with the pattern
-      local branch_re = '^cr/(%w+)(%-?%d+)'
-      local proj, issue = branch:match(branch_re)
-
-      -- If match found, format the output
-      if proj and issue then
-        return string.upper(proj) .. issue .. ': '
-      else
-        return 'Invalid branch format'
-      end
-    end, {}),
-  }, {
-    desc = 'commit prefix from branch name',
-  }),
+  s(
+    'br',
+    fmta(
+      [[
+      <1>:
+      ]],
+      {
+        f(get_ticket, {}),
+      },
+      {}
+    ),
+    {
+      desc = 'commit prefix from branch name',
+    }
+  ),
+  s(
+    'conv',
+    fmta(
+      [[
+      <1>(<2>): <3>
+      ]],
+      {
+        c(1, { t('feat'), t('chore'), t('fix'), t('docs'), t('refactor') }),
+        i(2),
+        i(3),
+      },
+      {}
+    ),
+    {
+      desc = 'conventional commit message',
+    }
+  ),
+  s(
+    'nv',
+    fmta(
+      [[
+      q
+      ]],
+      {
+        nodes,
+      },
+      {}
+    ),
+    {
+      desc = 'description',
+    }
+  ),
 }
