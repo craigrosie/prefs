6842df048e562af1541e61ee034f1c83d2439f5f
Craig Rosie <craigrosie7@gmail.com>
2026-06-26 16:25:03 +0100

    NEOVIM: Fix cmp

--- neovim/lua/kickstart/plugins/cmp.lua
+++ neovim/lua/kickstart/plugins/cmp.lua
@@ -55,7 +55,7 @@ return {
 
       -- Recommended by copilot-cmp
       local has_words_before = function()
-        if vim.vo[0].buftype == 'prompt' then
+        if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
           return false
         end
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
